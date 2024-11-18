9b47382731722dd4b5ba6599004d5c88d673763b
Samarth Kapoor <kapoorsamarth7@gmail.com>
2024-11-19 03:35:28 +0530

    done

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,7 @@
+"use client";
 import CheckIcon from '@/assets/check.svg'
 import { twMerge } from "tailwind-merge"
+import { motion } from 'framer-motion';
 
 const pricingTiers = [
   {
@@ -71,16 +73,26 @@ export const Pricing = () => {
             buttonText, 
             popular, inverse, 
             features 
-          }) => (
-        <div className={twMerge('card', inverse === true && 'border-black bg-black text-white'
+          }, index) => (
+        <div key={`tier-${index}`} className={twMerge('card', inverse === true && 'border-black bg-black text-white'
         )}>
           <div className='flex justify-between'>
             <h3 className={twMerge('text-lg font-bold text-black/50', inverse === true && "text-white/60")}>{title}</h3>
             {popular === true && (
               <div className='inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20'>
-                <span className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium'>
+                <motion.span 
+                  animate={{
+                    backgroundPositionX: "100%",
+                  }}
+                  transition={{
+                    duration: 1,
+                    repeat: Infinity,
+                    ease: "linear",
+                    repeatType: "loop"
+                  }}
+                className='bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF)] [background-size:200%] text-transparent bg-clip-text font-medium'>
                   Popular
-                </span>
+                </motion.span>
               </div>
             )}
           </div>
@@ -96,8 +108,8 @@ export const Pricing = () => {
             {buttonText}
           </button>
           <ul className='flex flex-col gap-5 mt-8'>
-            {features.map((feature) => (
-              <li className='text-sm flex items-center gap-4'>
+            {features.map((feature, featureIndex) => (
+               <li key={`feature-${index}-${featureIndex}`} className="text-sm flex items-center gap-4">
                 <CheckIcon className="h-6 w-6" />
                 <span>{feature}</span>
               </li>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,4 @@
+"use client";
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -9,6 +10,8 @@ import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
+import React from "react";
 
 const testimonials = [
   {
@@ -71,20 +74,40 @@ const firstColumn = testimonials.slice(0,3);
 const secondColumn = testimonials.slice(3,6);
 const thirdColumn = testimonials.slice(6,9);
 
-const TestimonialsColumn = (props: { className?: string; testimonials: typeof testimonials }) => (
-  <div className={twMerge("flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]", props.className)}>
-    {props.testimonials.map(({ text, imageSrc, name, username }) => (
-      <div className="card">
-        <div>{text}</div>
-          <div className="flex items-center gap-2 mt-5">
-            <Image src={imageSrc} alt={name} width={40} height={40} className="h-10 w-10 rounded-full" />
-            <div className="flex flex-col">
-              <div className="font-medium tracking-tight leading-5">{name}</div>
-              <div className="leading-5 tracking-tight">{username}</div>
+const TestimonialsColumn = (props: {
+  className?: string; 
+  testimonials: typeof testimonials;
+  duration?: number
+}) => (
+  <div className={props.className}>
+    <motion.div animate={{
+      translateY: "-50%",
+    }}
+      transition={{
+        repeat: Infinity,
+        duration: props.duration || 10,
+        ease: "linear",
+        repeatType: "loop",
+      }}
+      className="flex flex-col gap-6 pb-6"
+    >
+      {[...new Array(2)].fill(0).map((_, index) => (
+        <React.Fragment key={`loop-${index}`}>
+          {props.testimonials.map(({ text, imageSrc, name, username }, testimonialIndex) => (
+            <div key={`testimonial-${index}-${testimonialIndex}`} className="card">
+              <div>{text}</div>
+                <div className="flex items-center gap-2 mt-5">
+                  <Image src={imageSrc} alt={name} width={40} height={40} className="h-10 w-10 rounded-full" />
+                  <div className="flex flex-col">
+                    <div className="font-medium tracking-tight leading-5">{name}</div>
+                    <div className="leading-5 tracking-tight">{username}</div>
+                  </div>
+                </div>
             </div>
-          </div>
-      </div>
-    ))}
+          ))}
+        </React.Fragment>
+      ))}
+    </motion.div>
   </div>
 )
 
@@ -101,10 +124,10 @@ export const Testimonials = () => {
             From intuitive design to powerful features, our app has become an essential tool for users around the world.
           </p>
         </div>
-        <div className="flex justify-center gap-6">
-          <TestimonialsColumn testimonials={firstColumn} />
-          <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex" />
-          <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex" />
+        <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[738px] overflow-hidden">
+          <TestimonialsColumn testimonials={firstColumn} duration={15}/>
+          <TestimonialsColumn testimonials={secondColumn} className="hidden md:block" duration={19} />
+          <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:block" duration={17}/>
         </div>
       </div>
     </section>
